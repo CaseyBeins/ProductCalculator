7aabb9efc538096145e21b4cd4f2841d4e003219
Casey Beins <caseyleebeins@gmail.com>
2024-06-29 13:02:24 -0500

    Implemented try-with-resources pattern for Scanner

--- src/main/java/com/example/productcalculator/ProductCalculator.java
+++ src/main/java/com/example/productcalculator/ProductCalculator.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 public class ProductCalculator {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[5];
+        try (Scanner scanner = new Scanner(System.in)) {
+            int[] numbers = new int[5];
 
-        System.out.println("Enter five numbers:");
-        for (int i = 0; i < 5; i++) {
-            numbers[i] = scanner.nextInt();
+            System.out.println("Enter five numbers:");
+            for (int i = 0; i < 5; i++) {
+                numbers[i] = scanner.nextInt();
+            }
+
+            int product = calculateProduct(numbers, 5);
+            System.out.println("The product of the entered numbers is: " + product);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        int product = calculateProduct(numbers, 5);
-        System.out.println("The product of the entered numbers is: " + product);
     }
 
     // Recursive method to calculate product
@@ -22,7 +25,8 @@ public class ProductCalculator {
         if (n == 1) {
             return numbers[0];
         } else {
-            return numbers[n-1] * calculateProduct(numbers, n - 1);
+            return numbers[n - 1] * calculateProduct(numbers, n - 1);
         }
     }
 }
+
